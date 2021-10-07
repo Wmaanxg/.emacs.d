ca40e240b65451e469a204905d8f9e85b76fcfe9
Max <maxwizdomer@gmail.com>
2021-10-07 10:53:43 -0400

    configure helm

--- init.el
+++ init.el
@@ -23,8 +23,13 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-;; install helm
+;; install and configure helm
 (straight-use-package 'helm)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(setq helm-split-window-in-side-p t) ; open helm buffer inside current window, not occupy whole other window
+(helm-mode 1)
 
 (package-initialize)
 

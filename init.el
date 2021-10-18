c5fcf99ebd0554b540848ab1d115750fc6a3f291
Max <maxwizdomer@gmail.com>
2021-10-17 22:59:43 -0400

    add extra function for dired and suppress spash screen on startup

--- init.el
+++ init.el
@@ -76,3 +76,13 @@
 ;; set theme
 (load-theme 'gruvbox-dark-soft t)
 
+;; Suppress welcome screen
+(setq inhibit-startup-screen t)
+
+;; Extra function for dired to open files in other frame
+(defun dired-find-file-other-frame ()
+  "In Dired, visit this file or directory in another window."
+  (interactive)
+  (find-file-other-frame (dired-get-file-for-visit)))
+(eval-after-load "dired"
+  '(define-key dired-mode-map "F" 'dired-find-file-other-frame))
